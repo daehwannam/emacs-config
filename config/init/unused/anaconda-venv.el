1144089eb591c3719d6decfdda47aecaad037ce5
Daehwan Nam <dhnam@postech.ac.kr>
2017-06-22 12:45:55 +0900

    anaconda setting

--- config/init/unused/anaconda-venv.el
+++
@@ -1,5 +0,0 @@
-;;; virtual environment setting
-;; http://emacs.stackexchange.com/questions/20092/using-conda-environments-in-emacs
-;; usage: M-x pyvenv-workon virtual_env_name (ex. tensorflow)
-(setenv "WORKON_HOME" "~/bin/anaconda2/envs/")
-(pyvenv-mode 1)
