667e7934ba0b25f3f0261dfd1824d4d5fa319fd0
Daehwan Nam <dhnam@postech.ac.kr>
2017-06-24 21:31:12 +0900

    pyvenv

--- config/init/elpy.el
+++ config/init/elpy.el
@@ -8,6 +8,20 @@
 	(elpy-enable)
 	(setq elpy-rpc-backend "jedi")
 	;(setq elpy-rpc-python-command "python3")
+
+	(let (domain_name (get-string-from-file "~/.emacs.d/config/domain.txt"))
+	  (cond
+	   ;; ((string-equal domain_name "ms\n") ; Microsoft Windows
+	   ;;  (progn
+	   ;;    (do-something bla bla bla)))
+	   ;; ((string-equal domain_name "vbox\n") ; vbox linux
+	   ;;  (progn
+	   ;;    (do-something bla bla bla)))
+	   ((string-equal system-type "gnu/linux") ; linux
+	    (progn
+	      (setenv "WORKON_HOME" "~/bin/anaconda2/envs/")
+	      (pyvenv-mode 1)))))
+
 	)))
 
 ;;; For conda environment, "source activate <env>" should be done before running emacs.
