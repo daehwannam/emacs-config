9d94b5fa699475e2ee5b36d0e35f10018a3d4d11
Daehwan Nam <dhnam@postech.ac.kr>
2017-06-27 00:39:20 +0900

    venv

---
+++ config/script/fun/animate.el
@@ -0,0 +1,9 @@
+(defun animate-something ()
+  (interactive)
+  (let ((buffer (buffer-name)))
+    (switch-to-buffer (get-buffer-create "*Animation*"))
+    (erase-buffer)
+    (sit-for 0)
+    (animate-string "Amazing physics going on..."
+		    (/ (window-height) 2) (- (/ (window-width) 2) 12))
+    (switch-to-buffer buffer)))
