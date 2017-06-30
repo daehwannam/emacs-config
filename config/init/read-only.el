5925e33d2d1e1bbfd9e50066f1ded846e147afa6
Daehwan Nam <dhnam@postech.ac.kr>
2017-06-30 15:30:35 +0900

    read-only.el

---
+++ config/init/read-only.el
@@ -0,0 +1,3 @@
+(add-to-list 'auto-mode-alist '("\\.gz\\'" . read-only-mode))
+(add-to-list 'auto-mode-alist '("\\.zip\\'" . read-only-mode))
+(add-to-list 'auto-mode-alist '("\\.tar\\..*\\'" . read-only-mode))
