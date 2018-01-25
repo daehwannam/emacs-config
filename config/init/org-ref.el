d156f326848a9bd3befdffdf43b224037ce26289
Daehwan Nam <dhnam@postech.ac.kr>
2018-01-25 17:36:00 +0900

    org-ref is conditionally loaded

--- config/init/org-ref.el
+++ config/init/org-ref.el
@@ -1,19 +1,27 @@
-(require 'org-ref)
 
-(setq reftex-default-bibliography '("~/data/Dropbox/org/bibliography/references.bib"))
+(setq domain-name (trim-string (get-string-from-file "~/.emacs.d/config/domain.txt")))
+(cond
+ ((string-equal domain-name "vbox") ; vbox linux
+  (progn
+    (require 'org-ref)
 
-;; see org-ref for use of these variables
-(setq org-ref-bibliography-notes "~/data/Dropbox/org/bibliography/notes.org"
-      org-ref-default-bibliography '("~/data/Dropbox/org/bibliography/references.bib")
-      org-ref-pdf-directory "~/data/Dropbox/org/bibliography/bibtex-pdfs/")
+    (setq reftex-default-bibliography '("~/data/Dropbox/org/bibliography/references.bib"))
 
-;; citation key setting
-(setq bibtex-completion-bibliography "~/data/Dropbox/org/bibliography/references.bib"
-      bibtex-completion-library-path "~/data/Dropbox/org/bibliography/bibtex-pdfs"
-      bibtex-completion-notes-path "~/data/Dropbox/org/bibliography/helm-bibtex-notes")
+    ;; see org-ref for use of these variables
+    (setq org-ref-bibliography-notes "~/data/Dropbox/org/bibliography/notes.org"
+	  org-ref-default-bibliography '("~/data/Dropbox/org/bibliography/references.bib")
+	  org-ref-pdf-directory "~/data/Dropbox/org/bibliography/bibtex-pdfs/")
 
-;; open pdf with system pdf viewer (works on mac)
-(setq bibtex-completion-pdf-open-function
-  (lambda (fpath)
-    (start-process "open" "*open*" "open" fpath)))
+    ;; citation key setting
+    (setq bibtex-completion-bibliography "~/data/Dropbox/org/bibliography/references.bib"
+	  bibtex-completion-library-path "~/data/Dropbox/org/bibliography/bibtex-pdfs"
+	  bibtex-completion-notes-path "~/data/Dropbox/org/bibliography/helm-bibtex-notes")
+
+    ;; open pdf with system pdf viewer (works on mac)
+    (setq bibtex-completion-pdf-open-function
+	  (lambda (fpath)
+	    (start-process "open" "*open*" "open" fpath)))
+    )))
+
+
 
