71c6ebfa821620e465ae923e529174542aefa037
Daehwan Nam <dhnam@postech.ac.kr>
2018-01-28 23:45:48 +0900

    highlight the current line with keeping syntax

--- config/init/highlight_current.el
+++ config/init/highlight_current.el
@@ -7,3 +7,14 @@
 ;;; configuration
 ;;https://stackoverflow.com/questions/17701576/changing-highlight-line-color-in-emacs
 ;(global-hl-line-mode t)
+
+
+;;; hlt-highlight-symbol
+;;; https://emacs.stackexchange.com/questions/23958/combine-highlight-symbol-mode-and-hl-line-mode
+;; (require 'highlight)
+;; (hlt-highlight-symbol)
+
+
+;;; highlight with keeping syntax coloring
+(global-hl-line-mode 1)
+(set-face-foreground 'hl-line nil)
