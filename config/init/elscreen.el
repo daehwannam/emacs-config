e6ab45f30701ab1786b952b3dd756fe913fd8401
Daehwan Nam <dhnam@postech.ac.kr>
2018-04-08 04:55:29 +0900

    elscreen displays a title in a frame.

--- config/init/elscreen.el
+++ config/init/elscreen.el
@@ -97,3 +97,33 @@
 	    (setq buffers (cdr buffers)))
 	  (setq screens (cdr screens))))))
   )
+
+(global-set-key (kbd "C-c S") 'elscreen-store)
+(global-set-key (kbd "C-c F") 'elscreen-restore)
+
+
+;;; https://stackoverflow.com/questions/13176843/how-to-put-elscreen-tabs-on-the-top-only
+(setq elscreen-display-tab nil) ; disable tabs display
+
+;; get-alist was removed somewhere along the line
+;; You can try substituting all instances of get-alist with assoc-default
+;; instead of using defalias and see if that works; I haven't tried.
+(defalias 'get-alist 'assoc-default) ; get-alist is gone
+
+;; Put tabs display in your frame title bar instead.
+(defun elscreen-frame-title-update ()
+  (when (elscreen-screen-modified-p 'elscreen-frame-title-update)
+    (let* ((screen-list (sort (elscreen-get-screen-list) '<))
+           (screen-to-name-alist (elscreen-get-screen-to-name-alist))
+           (title (concat "| " (mapconcat
+                   (lambda (screen)
+                     (format "%d%s %s |"
+                             screen (elscreen-status-label screen)
+                             (get-alist screen screen-to-name-alist)))
+                   screen-list " "))))
+      (if (fboundp 'set-frame-name)
+          (set-frame-name title)
+        (setq frame-title-format title)))))
+
+(eval-after-load "elscreen"
+  '(add-hook 'elscreen-screen-update-hook 'elscreen-frame-title-update))
