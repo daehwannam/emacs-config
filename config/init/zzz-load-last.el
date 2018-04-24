02727fb908e5bc39d8ac739b42d8650890b8b442
Daehwan Nam <dhnam@postech.ac.kr>
2018-04-24 16:38:08 +0100

    zzz-load-last.el is added.

---
+++ config/init/zzz-load-last.el
@@ -0,0 +1,4 @@
+
+(load "~/.emacs.d/config/init/theme.el")
+(load "~/.emacs.d/config/init/highlight_current.el")
+
--- package.el
+++ package.el
@@ -18,7 +18,7 @@
   (cond
    ((string-equal machine-domain "default") '())
    ((string-equal machine-domain "basic") '(vlf magit workgroups2))
-   ((string-equal machine-domain "vbox") '(vlf auctex magit material-theme elpy flycheck py-autopep8 workgroups2))
+   ((string-equal machine-domain "vbox") '(vlf auctex magit material-theme elpy flycheck py-autopep8 workgroups2 latex-preview-pane))
    ((string-equal machine-domain "engels") '(vlf magit elpy flycheck py-autopep8 workgroups2))
    ;; ((string-equal machine-domain "engels") '(vlf magit elpy py-autopep8))
    ((string-equal machine-domain "programming") '(elpy))
