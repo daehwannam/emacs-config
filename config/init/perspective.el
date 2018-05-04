a1d682fa35f027ff7fc91dc3d9aa5c2829bbe711
Daehwan Nam <dhnam@postech.ac.kr>
2018-05-04 07:49:44 +0100

    perspective.el: key bindings are changed.

--- config/init/perspective.el
+++ config/init/perspective.el
@@ -1,9 +1,13 @@
 
 (require 'make-repeatable-command)
 
-(persp-mode)
+;;; change prefix-key
+(setq persp-mode-prefix-key (kbd "C-z"))
 
-(defvar persp-count 0
+;;; change default perspective name
+(setq persp-initial-frame-name "p0")	;default = main
+
+(defvar persp-count 1
   "Counter for the created perspectives")
 
 (defvar default-persp-name "p")
@@ -11,9 +15,12 @@
 (defun next-persp-name ()
   (concat default-persp-name (number-to-string persp-count)))
 
-;;; rename the first perspective
-(persp-rename (next-persp-name))
-(setq persp-count (1+ persp-count))
+;; ;;; rename the first perspective
+;; (persp-rename (next-persp-name))
+;; (setq persp-count (1+ persp-count))
+
+;;; enable persp-mode
+(persp-mode)
 
 ;;; open new perspective (modified version of persp-switch
 (defun persp-open (name)
@@ -30,11 +37,13 @@
 (define-key perspective-map (kbd "n") (make-repeatable-command'persp-next))
 (define-key perspective-map (kbd "p") (make-repeatable-command'persp-prev))
 
-(define-key perspective-map (kbd "o") (make-repeatable-command'persp-next))
-(define-key perspective-map (kbd "O") (make-repeatable-command'persp-prev))
+(define-key perspective-map (kbd "o") 'persp-open)
 
-(define-key perspective-map (kbd "ㅐ") (make-repeatable-command'persp-next))
-(define-key perspective-map (kbd "ㅒ") (make-repeatable-command'persp-prev))
+;; (define-key perspective-map (kbd "o") (make-repeatable-command'persp-next))
+;; (define-key perspective-map (kbd "O") (make-repeatable-command'persp-prev))
+
+;; (define-key perspective-map (kbd "ㅐ") (make-repeatable-command'persp-next))
+;; (define-key perspective-map (kbd "ㅒ") (make-repeatable-command'persp-prev))
 
 ;; (global-set-key (kbd "C-c 2") 'persp-open)
 
@@ -44,3 +53,15 @@
 ;; (global-set-key (kbd "C-c ㅐ") (make-repeatable-command'persp-next))
 ;; (global-set-key (kbd "C-c ㅒ") (make-repeatable-command'persp-prev))
 
+
+
+;;;; [default Key bindings]
+;;;; s -- persp-switch: Query a perspective to switch or create
+;;;; k -- persp-remove-buffer: Query a buffer to remove from current perspective
+;;;; c -- persp-kill : Query a perspective to kill
+;;;; r -- persp-rename: Rename current perspective
+;;;; a -- persp-add-buffer: Query an open buffer to add to current perspective
+;;;; A -- persp-set-buffer: Add buffer to current perspective and remove it from all others
+;;;; i -- persp-import: Import a given perspective from another frame.
+;;;; n, <right> -- persp-next : Switch to next perspective
+;;;; p, <left> -- persp-prev: Switch to previous perspective
