049582f99d0361d8f0d70e4920afaf0d17d379ef
Daehwan Nam <dhnam@postech.ac.kr>
2018-06-19 17:00:37 +0900

    simple-counsel.el is modified.

--- config/init/simple-counsel.el
+++ config/init/simple-counsel.el
@@ -8,6 +8,7 @@
 
   ;; Ivy-based interface to standard commands
   (global-set-key (kbd "C-c s") 'swiper)
+  (global-set-key (kbd "C-c C-s") 'swiper)
   ;; (global-set-key (kbd "C-r") 'swiper)
   (global-set-key (kbd "M-x") 'counsel-M-x)
   ;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
