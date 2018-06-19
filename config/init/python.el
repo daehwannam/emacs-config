23348059e0a6ff9126ab45fb16d3103f40ed9174
Daehwan Nam <dhnam@postech.ac.kr>
2018-06-20 01:33:51 +0900

    py-repl-kill-ring-save is added to python-mode-hook.

--- config/init/python.el
+++ config/init/python.el
@@ -116,3 +116,7 @@
 		     (prefix-numeric-value current-prefix-arg)))
   (kill-new (elpy-shell--region-without-indentation beg end))
   (setq deactivate-mark t))
+
+;; https://stackoverflow.com/a/14230685
+(add-hook 'python-mode-hook
+	  (lambda () (local-set-key (kbd "C-c M-w") #'py-repl-kill-ring-save)))
