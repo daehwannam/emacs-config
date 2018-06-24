cc2499d917e2468a0b09839602f599bea40b827a
Daehwan Nam <dhnam@postech.ac.kr>
2018-06-25 01:44:17 +0900

    compilation.el is added, including 'o' binding for grep.

---
+++ config/init/compilation.el
@@ -0,0 +1,15 @@
+
+;; https://emacs.stackexchange.com/a/33908
+
+(defun my-compile-goto-error-same-window ()
+  (interactive)
+  (let ((display-buffer-overriding-action
+         '((display-buffer-reuse-window
+            display-buffer-same-window)
+           (inhibit-same-window . nil))))
+    (call-interactively #'compile-goto-error)))
+
+(defun my-compilation-mode-hook ()
+  (local-set-key (kbd "o") #'my-compile-goto-error-same-window))
+
+(add-hook 'compilation-mode-hook #'my-compilation-mode-hook)
