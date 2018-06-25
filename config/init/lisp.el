ad856d59946cbe2128f98a21547ef9f7bd2ca4c4
Daehwan Nam <dhnam@postech.ac.kr>
2018-06-25 09:53:04 +0900

    lisp.el is modified.

--- config/init/lisp.el
+++ config/init/lisp.el
@@ -1,7 +1,8 @@
 
 ;; Emacs lisp 
 (electric-indent-mode -1)
-
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda () (local-set-key (kbd "C-j") #'eval-print-last-sexp)))
 (add-to-list 'auto-mode-alist '("\\.el\\.gz\\'" . emacs-lisp-mode))
 ;; (add-to-list 'auto-mode-alist '(".el\\.gz\\'" . emacs-lisp-mode))
 
@@ -9,11 +10,21 @@
 ;; Common lisp
 (let ((path-to-slime-helper "~/quicklisp/slime-helper.el")
       (path-to-inferior-lisp-program "/usr/bin/sbcl"))
+  (cond
+   ((string-equal machine-domain "vbox")
+    (setq path-to-slime-helper "~/quicklisp/slime-helper.el")
+    (setq path-to-inferior-lisp-program "/usr/bin/sbcl"))
+   (t
+    (setq path-to-slime-helper "~/NO-PATH-POSSIBLE")
+    (setq path-to-inferior-lisp-program "/NO-PATH-POSSIBLE")))    
   (when (file-exists-p  path-to-slime-helper)
     (load (expand-file-name path-to-slime-helper)))
     ;; Replace "sbcl" with the path to your implementation
   (when (file-exists-p path-to-inferior-lisp-program)
-    (setq inferior-lisp-program path-to-inferior-lisp-program)))
+    (setq inferior-lisp-program path-to-inferior-lisp-program))
+  (when (fboundp 'slime)
+    (add-hook 'slime-mode-hook
+	      (lambda () (local-set-key (kbd "C-j") #'slime-eval-print-last-expression)))))
 
 ;; ;; ;; ParEdit
 ;; ;; ;; http://wikemacs.org/wiki/Paredit-mode
