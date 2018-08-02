cfa085ab5b3afee0583e4c903f2612a5f27ae4d1
Daehwan Nam <dhnam@postech.ac.kr>
2018-08-02 21:07:24 +0900

    serveral changes

--- config/init/custom-setting.el
+++ config/init/custom-setting.el
@@ -1,11 +1,13 @@
 ;;; customization file setting
 ;; http://stackoverflow.com/questions/5052088/what-is-custom-set-variables-and-faces-in-my-emacs
 
-(let ((custom-file-name "~/.emacs.d/config/custom/custom.el"))
-  (unless (file-exists-p custom-file-name)
-    ;; https://stackoverflow.com/questions/17376706/in-emacs-lisp-how-can-i-append-a-string-to-a-file-that-i-dont-like-to-open
-    ;; https://stackoverflow.com/questions/14071991/how-to-create-an-empty-file-by-elisp/14072295
-    (write-region "" nil custom-file-name))
+(when (not (string-equal system-type "windows-nt")) ; Microsoft Windows
+  ;; http://ergoemacs.org/emacs/elisp_determine_OS_version.html
+  (let ((custom-file-name "~/.emacs.d/config/custom/custom.el"))
+    (unless (file-exists-p custom-file-name)
+      ;; https://stackoverflow.com/questions/17376706/in-emacs-lisp-how-can-i-append-a-string-to-a-file-that-i-dont-like-to-open
+      ;; https://stackoverflow.com/questions/14071991/how-to-create-an-empty-file-by-elisp/14072295
+      (write-region "" nil custom-file-name))
 
-  (setq custom-file custom-file-name)
-  (load custom-file))
+    (setq custom-file custom-file-name)
+    (load custom-file)))
--- config/init/elpy.el
+++ config/init/elpy.el
@@ -22,19 +22,42 @@
        ;;  (progn
        ;;    (do-something bla bla bla)))
        ((string-equal machine-domain "hegel") ; hegel
-	(progn
-	  (setenv "WORKON_HOME" "~/bin/anaconda2/envs/")
-	  (pyvenv-mode 1)))
+	(setenv "WORKON_HOME" "~/bin/anaconda2/envs/")
+	(pyvenv-mode 1))
        ((string-equal machine-domain "engels") ; engels
-	(progn
-	  (setenv "WORKON_HOME" "~/bin/anaconda3/envs/")
-	  (pyvenv-mode 1)
-	  (pyvenv-workon "py3")))
+	(setenv "WORKON_HOME" "~/bin/anaconda3/envs/")
+	(pyvenv-mode 1)
+	(pyvenv-workon "py3"))		       ; use "M-x pyvenv-deactivate" to deactivate
        ((string-equal machine-domain "vbox") ; vbox
-	(progn
-	  (setenv "WORKON_HOME" "~/bin/anaconda3/envs/")
-	  (pyvenv-mode 1)))
+	(setenv "WORKON_HOME" "~/bin/anaconda3/envs/")
+	(pyvenv-mode 1))
        )
+
+      (defun elpy-set-project-root (new-root) ; redefined
+	"Set the Elpy project root to NEW-ROOT."
+	(interactive "DNew project root: ")
+	(setq elpy-project-root (file-truename new-root)))
+
+      
+      (defun elpy-shell-switch-to-shell-with-new-root (new-root)
+	"Switch to inferior Python process buffer."
+	(interactive "DNew project root: ")
+	(setq elpy-project-root (file-truename new-root)) ; modified from 'elpy-set-project-root
+	(setq elpy--shell-last-py-buffer (buffer-name))
+	(pop-to-buffer (process-buffer (elpy-shell-get-or-create-process)))
+
+;; 	(let ((dir-path default-directory))
+;; 	  (setq elpy--shell-last-py-buffer (buffer-name))
+;; 	  (pop-to-buffer (process-buffer (elpy-shell-get-or-create-process)))
+;; 	  (insert (concat "import sys
+;; sys.path.append('" dir-path "')"))
+;; 	  (comint-send-input))
+	)
+
+      ;; (add-hook 'python-mode-hook
+      ;; 		(lambda () (local-set-key (kbd "C-c Z") 'elpy-shell-switch-to-shell-with-new-root)))
+      (add-hook 'python-mode-hook
+		(lambda () (local-set-key (kbd "C-c Z") 'elpy-set-project-root)))
 
       ;; https://realpython.com/blog/python/emacs-the-best-python-editor/
       (unless (member machine-domain '("vbox" "machine here don't activate below"))
