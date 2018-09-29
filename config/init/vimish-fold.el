532144b54af5990575b5b1126c917adf1ec73b14
Daehwan Nam <dhnam@postech.ac.kr>
2018-09-30 03:47:25 +0900

    vimish-fold.el is added

---
+++ config/init/vimish-fold.el
@@ -0,0 +1,18 @@
+(require 'vimish-fold)
+(vimish-fold-global-mode 1)
+
+
+(global-set-key (kbd "C-c i f") #'vimish-fold)
+(global-set-key (kbd "C-c i d") #'vimish-fold-delete)
+
+(global-set-key (kbd "C-c i u") #'vimish-fold-unfold)
+(global-set-key (kbd "C-c i U") #'vimish-fold-unfold-all)
+(global-set-key (kbd "C-c i r") #'vimish-fold-refold)
+(global-set-key (kbd "C-c i R") #'vimish-fold-refold-all)
+(global-set-key (kbd "C-c i D") #'vimish-fold-delete-all)
+(global-set-key (kbd "C-c i t") #'vimish-fold-toggle)
+(global-set-key (kbd "C-c i T") #'vimish-fold-toggle-all)
+
+(global-set-key (kbd "C-c i n") #'vimish-fold-next-fold)
+(global-set-key (kbd "C-c i p") #'vimish-fold-previous-fold)
+
