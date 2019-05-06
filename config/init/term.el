8afd6c27e994a5b581f7d24cbf70dc65deeb2edb
Daehwan Nam <dhnam@postech.ac.kr>
2019-05-07 00:56:18 +0900

    tmux-term is added

--- config/init/term.el
+++ config/init/term.el
@@ -3,3 +3,21 @@
 (require 'named-term)
 
 (defalias 'mterm 'multi-term)
+
+(defun tmux-term ()
+  "Create new term buffer, then do tmux new session.
+It's modified code of 'multi-term.
+Will prompt you shell name when you type `C-u' before this command."
+  (interactive)
+  (let (term-buffer)
+    ;; Set buffer.
+    (setq term-buffer (multi-term-get-buffer current-prefix-arg))
+    (setq multi-term-buffer-list (nconc multi-term-buffer-list (list term-buffer)))
+    (set-buffer term-buffer)
+    ;; Internal handle for `multi-term' buffer.
+    (multi-term-internal)
+    ;; Switch buffer
+    (switch-to-buffer term-buffer))
+  (term-send-raw-string "tmux new \C-m"))
+
+(defalias 'tterm 'tmux-term)
