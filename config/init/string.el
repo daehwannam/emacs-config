e783ed5d759e6abb4ac78cc893bb88799baebd53
Daehwan Nam <dhnam@postech.ac.kr>
2019-09-17 17:24:41 +0900

    round-number is added

---
+++ config/init/string.el
@@ -0,0 +1,20 @@
+
+;; https://stackoverflow.com/questions/23636226/how-to-round-all-the-numbers-in-a-region
+(defun round-number (start end &optional fp)
+  "round the numbers of region."
+  (interactive "r")
+  ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Text-from-Minibuffer.html
+  (let ((fp (or fp (read-string "Enter floating point format (ex. 0.3): "))))
+    (save-restriction
+      (narrow-to-region start end)
+      (goto-char 1)
+      (let ((case-fold-search nil))
+	(while (search-forward-regexp "\\([0-9]+\\.[0-9]+\\)" nil t)
+	  (replace-match (format (concat "%" fp "f") (string-to-number (match-string 1)))
+			 ))))))
+
+
+;; round by query-replace-regexp
+;; https://stackoverflow.com/a/23644015
+;;
+;; C-M-% -> [0-9]+\.[0-9]+ -> RET -> \,(format "%0.2f" \#&) -> RET
