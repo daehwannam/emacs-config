eefcd04b1061da7a97db62d228bae11a6fef276f
Daehwan Nam <dhnam@postech.ac.kr>
2020-06-01 15:01:42 +0900

    hy repr sertup is updated

--- config/init/python.el
+++ config/init/python.el
@@ -163,7 +163,7 @@
       ;; afterwards, it raises not errors
       (run-jedhy)
     (error nil))
-  ;; (setq hy-shell--interpreter-args '("--spy" "--repl-output-fn" "hy.contrib.hy-repr.hy-repr"))  # simplified representation
+  (setq hy-shell--interpreter-args '("--spy" "--repl-output-fn" "hy.contrib.hy-repr.hy-repr"))  # simplified representation
   ;; (setq hy-shell--interpreter-args '("--spy"))  ; default arguments
   )
 
