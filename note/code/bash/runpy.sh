a26bfdcb58aed8a7a490549073a69f673d89e5c2
Daehwan Nam <dhnam@postech.ac.kr>
2020-10-24 22:31:58 +0900

    runpy.sh & runhy.sh are updated

---
+++ note/code/bash/runpy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# execute "chmod a+x run.sh" for convenience
+
+argv=("$@")
+path=${argv[0]}
+
+py_module=${path//\//.}  # replace '/' with '.'
+py_module=$(basename $py_module .py)
+py_args=${argv[@]:1}
+
+python -m $py_module ${py_args[@]}
