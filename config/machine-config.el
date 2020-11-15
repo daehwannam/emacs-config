00b424d6ca3303d569f38e11a99a22435df84e6c
Daehwan Nam <dhnam@postech.ac.kr>
2020-11-16 07:34:48 +0900

    multi-term error fixed

--- config/machine-config.el
+++ config/machine-config.el
@@ -43,12 +43,12 @@
      ;; (engles t)
      )
     (installable-packages
-     (basic (vlf magit counsel))
-     (vbox (vlf magit auctex latex-preview-pane material-theme elpy flycheck py-autopep8 sphinx-doc workgroups2 perspective counsel iedit wgrep which-key projectile flx-ido counsel-projectile eyebrowse persp-projectile paredit markdown-mode tuareg ein hy-mode)) ; removed: highlight
+     (basic (vlf magit multi-term counsel))
+     (vbox (vlf magit  multi-term auctex latex-preview-pane material-theme elpy flycheck py-autopep8 sphinx-doc workgroups2 perspective counsel iedit wgrep which-key projectile flx-ido counsel-projectile eyebrowse persp-projectile paredit markdown-mode tuareg ein hy-mode)) ; removed: highlight
      ;; (engels (vlf magit elpy flycheck py-autopep8 wgrep which-key projectile flx-ido  eyebrowse))
-     (engels (vlf magit elpy flycheck py-autopep8 wgrep which-key projectile flx-ido counsel counsel-projectile eyebrowse sphinx-doc markdown-mode hy-mode))
+     (engels (vlf magit  multi-term elpy flycheck py-autopep8 wgrep which-key projectile flx-ido counsel counsel-projectile eyebrowse sphinx-doc markdown-mode hy-mode))
      ;; (engels (vlf magit elpy py-autopep8))
-     (programming (vlf magit counsel elpy flycheck py-autopep8))
+     (programming (vlf magit multi-term counsel elpy flycheck py-autopep8))
      (ms-laptop (vlf magit))
      (ms-desktop (vlf magit))
      )))
