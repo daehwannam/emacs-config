ca2b15c7864492bc658d86395a55dab621d16f18
Daehwan Nam <dhnam@postech.ac.kr>
2021-01-11 11:27:36 +0900

    package related code is upcated

--- base/package-install.el
+++ base/package-install.el
@@ -5,14 +5,5 @@
   (defvar installable-domain-specific-packages) ; (defvar machine-domain-specific-packages some-value)
   (setq installable-domain-specific-packages (machine-config-get 'installable-packages))
 
-  (let ((package-content-refreshed nil))
-    (mapc #'(lambda (package)
-	      (unless (package-installed-p package)
-		(print "------------------------------------------------------------------")
-		(print package)
-		(unless package-content-refreshed
-		  ;; https://magit.vc/manual/magit/Installing-from-Melpa.html
-		  (package-refresh-contents)
-		  (setq package-content-refreshed t))
-		(package-install package)))
-	  installable-domain-specific-packages)))
+  (mapc #'install-package-unless-installed
+	installable-domain-specific-packages))
