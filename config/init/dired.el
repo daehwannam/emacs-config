3ea5d047eb967e094ae119365315a4497d3fa640
Daehwan Nam <dhnam@postech.ac.kr>
2021-02-06 00:25:12 +0900

    ora-dired-rsync is added

--- config/init/dired.el
+++ config/init/dired.el
@@ -184,3 +184,44 @@ Version 2019-11-04"
 
 (add-hook 'dired-mode-hook
 	  (lambda () (local-set-key (kbd "C-c C-o") 'xah-open-in-external-app)))
+
+
+(progn
+  ;; Copy files asynchronously
+  ;;
+  ;; https://stackoverflow.com/questions/379940/dired-copy-asynchronously
+  ;; https://oremacs.com/2016/02/24/dired-rsync/
+
+  (require 'dired-x)
+
+;;;###autoload
+  (defun ora-dired-rsync (dest)
+    (interactive
+     (list
+      (expand-file-name
+       (read-file-name
+	"Rsync to:"
+	(dired-dwim-target-directory)))))
+    ;; store all selected files into "files" list
+    (let ((files (dired-get-marked-files
+                  nil current-prefix-arg))
+          ;; the rsync command
+          (tmtxt/rsync-command
+           "rsync -arvz --progress "))
+      ;; add all selected file names as arguments
+      ;; to the rsync command
+      (dolist (file files)
+	(setq tmtxt/rsync-command
+              (concat tmtxt/rsync-command
+                      (shell-quote-argument file)
+                      " ")))
+      ;; append the destination
+      (setq tmtxt/rsync-command
+            (concat tmtxt/rsync-command
+                    (shell-quote-argument dest)))
+      ;; run the async shell command
+      (async-shell-command tmtxt/rsync-command "*rsync*")
+      ;; finally, switch to that window
+      (other-window 1)))
+
+  (define-key dired-mode-map "Y" 'ora-dired-rsync))
