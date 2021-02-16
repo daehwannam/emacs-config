e4ae9c579b179f76054d8659ab7845186e92b058
Daehwan Nam <dhnam@postech.ac.kr>
2021-02-17 02:34:07 +0900

    visual error of tab-bar-mode in terminal emacs is fixed

--- config/init/tab-bar.el
+++ config/init/tab-bar.el
@@ -4,6 +4,26 @@
   (defun tab-bar-move-tab-reverse (&optional arg)
     (interactive "p")
     (tab-bar-move-tab (- (or arg 1))))
+
+  (progn
+    ;; advice for tab-next and tab-previous
+    (defun tab-bar-switch-to-next-tab-advice (orig-fun &rest args)
+      (apply orig-fun args)
+      (progn
+	;; to fix the problem of disappeared text with vertical
+	;; windows when moving a cursor vertically after tab-next or
+	;; tab-previous in emacs terminal mode
+	;;
+	;; https://emacs.stackexchange.com/a/29226
+	(let ((seconds 0.01))
+	  ;; delayed command execution
+	  (run-with-timer seconds nil #'force-mode-line-update 't))
+	(comment (force-mode-line-update t))
+	(comment
+	 (dotimes (number (count-windows))
+	   (other-window 1)))))
+
+    (advice-add 'tab-bar-switch-to-next-tab :around #'tab-bar-switch-to-next-tab-advice))
     
   (progn
     (define-key tab-prefix-map "8" 'tab-new)
