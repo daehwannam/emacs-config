7e34899652613fa8225ff782af072a443d4546a1
Daehwan Nam <dhnam@postech.ac.kr>
2021-11-29 06:14:46 +0900

    exwm update

--- config/init/exwm.el
+++ config/init/exwm.el
@@ -1,19 +1,36 @@
-(let ((exwm-enabled (machine-config-get-first 'exwm-enabled)))
-  (when exwm-enabled
-   (defun exwm-simple-init ()
-     (progn
-       ;; disable scroll-bar, menu-bar, tool-bar
-       (toggle-scroll-bar -1)
-       (menu-bar-mode -1)
-       (tool-bar-mode -1))
-     (progn
-       (require 'exwm)
-       (require 'exwm-config)))
+(progn
+  (defvar exwm-cmd-arg-passed nil)
 
-   (defun exwm-config-mine ()
-     (exwm-simple-init)
-     (progn
-       (exwm-config-example)          ; same with (exwm-config-default)
-       (ido-mode nil))
-     )
-   (exwm-config-mine)))
+  (let ((remaining-cmd-args (cdr command-line-args)))
+    ;; originally '(cdr command-line-args) is passed into `command-line-1'
+    (setq exwm-cmd-arg-passed (member "--exwm" remaining-cmd-args)))
+
+  ;; exwm setup
+  (defun exwm-cmd-arg-handler (switch)
+    ;; do nothing
+    )
+
+  (add-to-list 'command-switch-alist '("--exwm" . exwm-cmd-arg-handler)))
+
+(when exwm-cmd-arg-passed
+  (let ((exwm-enabled (machine-config-get-first 'exwm-enabled)))
+    ;; conditional eval
+    (when exwm-enabled))
+
+  (defun exwm-simple-init ()
+    (progn
+      ;; disable scroll-bar, menu-bar, tool-bar
+      (toggle-scroll-bar -1)
+      (menu-bar-mode -1)
+      (tool-bar-mode -1))
+    (progn
+      (require 'exwm)
+      (require 'exwm-config)))
+
+  (defun exwm-config-mine ()
+    (exwm-simple-init)
+    (progn
+      (exwm-config-example)  ; same with (exwm-config-default)
+      (ido-mode nil)))
+
+  (exwm-config-mine))
