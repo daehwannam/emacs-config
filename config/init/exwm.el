e249270535469224c5cff3ed62ef56cd7279c01b
Daehwan Nam <dhnam@postech.ac.kr>
2021-11-28 22:31:28 +0900

    minor change

--- config/init/exwm.el
+++ config/init/exwm.el
@@ -1,14 +1,19 @@
 (let ((exwm-enabled (machine-config-get-first 'exwm-enabled)))
   (when exwm-enabled
-    (progn
-      ;; disable scroll-bar, menu-bar, tool-bar
-      (toggle-scroll-bar -1) ; 
-      (menu-bar-mode -1)
-      (tool-bar-mode -1))
+   (defun exwm-simple-init ()
+     (progn
+       ;; disable scroll-bar, menu-bar, tool-bar
+       (toggle-scroll-bar -1)
+       (menu-bar-mode -1)
+       (tool-bar-mode -1))
+     (progn
+       (require 'exwm)
+       (require 'exwm-config)))
 
-    (progn
-      (require 'exwm)
-      (require 'exwm-config)
-      ;; (exwm-config-example)
-      )
-    ))
+   (defun exwm-config-mine ()
+     (exwm-simple-init)
+     (progn
+       (exwm-config-example)          ; same with (exwm-config-default)
+       (ido-mode nil))
+     )
+   (exwm-config-mine)))
