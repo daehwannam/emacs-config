5039aa637b3ba1e1a26c7cc1db0a3cc0df411f63
Daehwan Nam <dhnam@postech.ac.kr>
2021-12-31 01:52:43 +0900

    error fix

--- config/init/zzz-load-last.el
+++ config/init/zzz-load-last.el
@@ -5,7 +5,8 @@
  (when (file-exists-p "~/.emacs.d/config/init/eyebrowse.el")
    (load "~/.emacs.d/config/init/eyebrowse.el")))
 
-(when (machine-config-get-first 'exwm-multiple-monitor-layout-type)
+(when (and exwm-cmd-arg-passed
+           (machine-config-get-first 'exwm-multiple-monitor-layout-type))
     ;; this prevent wrong window deployment when
     ;; `exwm-base-input-simulation-keys' has many commands
     (exwm-init))
