d0a27ae3093d42093ef5e1fa86f9cf908f19527d
Daehwan Nam <dhnam@postech.ac.kr>
2022-10-31 20:14:19 +0900

    tui update

--- config/init/tui.el
+++ config/init/tui.el
@@ -1,9 +1,12 @@
-(when (eq window-system nil)
-  ;; Enable mouse in terminal mode
-  ;; https://www.reddit.com/r/emacs/comments/5j89xn/comment/dbfvi21/?utm_source=share&utm_medium=web2x&context=3
-  (xterm-mouse-mode t))
+(when (eq window-system nil)  ; when it's in terminal mode
+  (when (package-installed-p 'xclip)
+    (progn
+      ;; Enable copy/paste between terminal emacs and gui applications
+      ;; https://stackoverflow.com/a/46219455
+      (xclip-mode t))
+    (progn
+      ;; Enable mouse in terminal mode
+      ;; https://www.reddit.com/r/emacs/comments/5j89xn/comment/dbfvi21/?utm_source=share&utm_medium=web2x&context=3
+      (xterm-mouse-mode t))))
 
-(when (package-installed-p 'xclip)
-  ;; Enable copy/paste between terminal emacs and gui applications
-  ;; https://stackoverflow.com/a/46219455
-  (xclip-mode t))
+
