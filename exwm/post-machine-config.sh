2219fbe7e1d8779c6aae7798729bcc41f99b94d1
Daehwan Nam <dhnam@postech.ac.kr>
2022-11-05 03:57:23 +0900

    laptop config update

--- exwm/post-machine-config.sh
+++ exwm/post-machine-config.sh
@@ -10,7 +10,13 @@ OPTION_PATH="${DIR_PATH}/option.txt"
 if [ -f $OPTION_PATH ]; then
     OPTION_VALUE=$(cat $OPTION_PATH)
     if [ $OPTION_VALUE = "laptop" ]; then
-        :  # pass
+        # xmodmap $DIR_PATH/dependent/ctrl-caps-swap.xmodmap
+
+        # # Adjust screen off time
+        # xset dpms 0 0 1800  # 1800 secs = 30 mins
+        xset s 1800 1800
+        xset dpms 2100 2400 2700
+        # :  # pass
     elif [ $OPTION_VALUE = "descartes" ]; then
         # xmodmap $DIR_PATH/dependent/ctrl-caps-swap.xmodmap
 
