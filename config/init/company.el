230b5a57d3fd4ec51041dad2c88c0fff94daf970
Daehwan Nam <dhnam@postech.ac.kr>
2022-11-25 22:40:10 +0900

    company mode is allowed to type non-matching characters

--- config/init/company.el
+++ config/init/company.el
@@ -3,4 +3,9 @@
   :config
   (progn
     (comment (add-hook 'after-init-hook 'global-company-mode))
-    (add-hook 'emacs-lisp-mode-hook 'company-mode)))
+    (add-hook 'emacs-lisp-mode-hook 'company-mode))
+
+  (progn
+    ;; allow to type non-matching characters
+    ;; https://emacs.stackexchange.com/a/18696
+    (setq company-require-match nil)))
