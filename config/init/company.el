2b6ff8ce1f790d1ff0b6f3048d0619d4c0407752
Daehwan Nam <dhnam@postech.ac.kr>
2022-11-27 01:51:34 +0900

    company-mode update

--- config/init/company.el
+++ config/init/company.el
@@ -1,11 +1,24 @@
 
 (use-existing-pkg company
-  :config
-  (progn
-    (comment (add-hook 'after-init-hook 'global-company-mode))
-    (add-hook 'emacs-lisp-mode-hook 'company-mode))
+  :bind
+  (nil
+   :map company-mode-map
 
+   ;; "C-M-i" is originally mapped to `completion-at-point'
+   ("C-M-i" . company-complete))
+
+  :config
   (progn
     ;; allow to type non-matching characters
     ;; https://emacs.stackexchange.com/a/18696
-    (setq company-require-match nil)))
+    (setq company-require-match nil))
+
+  :init
+  (progn
+    (add-hook 'after-init-hook 'global-company-mode)
+    (comment (add-hook 'emacs-lisp-mode-hook 'company-mode)))
+  (progn
+    ;; disable automatic pop-up
+    ;; https://stackoverflow.com/questions/69439076/how-to-prevent-automatic-auto-completion-window-pop-up-after-class-member-access
+    (setq company-idle-delay nil))
+  (comment (global-set-key (kbd "C-M-i") 'company-complete)))
