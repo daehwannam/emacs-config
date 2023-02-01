3dfe63163c55b4a6787e336a3d4f3e85480949f9
Daehwan Nam <dhnam@postech.ac.kr>
2023-02-01 14:28:41 +0900

    hydra update for key-chord

--- base/hydra-setup.el
+++ base/hydra-setup.el
@@ -5,3 +5,8 @@
   ;; Disable helpful messages for all hydra commands
   ;; https://github.com/abo-abo/hydra/issues/196#issuecomment-218900683
   (setq hydra-is-helpful nil))
+
+(progn
+  ;; Allow using key-chord
+  ;; https://github.com/abo-abo/hydra/issues/292#issuecomment-408130658
+  (defun hydra-default-pre ()))
