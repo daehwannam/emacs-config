df6e66dcc800f43519a21471c47fca66a1a7b56f
Daehwan Nam <dhnam@postech.ac.kr>
2023-02-01 14:28:54 +0900

    disable auth-source

--- config/init/tramp.el
+++ config/init/tramp.el
@@ -4,4 +4,15 @@
   ;; https://stackoverflow.com/a/61169654
   (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
 
+(progn
+  ;; auth-source
+  (progn
+    ;; authentication priorities
+    ;; "~/.authinfo" is not used
+    (setq auth-sources '("~/.authinfo.gpg" "~/.netrc")))
+  (progn
+    ;; disable to ask whether to save authentication information
+    ;; the value should be one of nil, t, or 'ask
+    (customize-save-variable 'auth-source-save-behavior nil)))
+
 (provide 'dhnam-tramp)
