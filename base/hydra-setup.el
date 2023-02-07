c8cfbed66bfe62022a148cf745e237251c8d0596
Daehwan Nam <dhnam@postech.ac.kr>
2023-02-08 06:36:09 +0900

    hydra key-chord fix

--- base/hydra-setup.el
+++ base/hydra-setup.el
@@ -6,7 +6,7 @@
   ;; https://github.com/abo-abo/hydra/issues/196#issuecomment-218900683
   (setq hydra-is-helpful nil))
 
-(progn
+(with-eval-after-load 'hydra
   ;; Allow using key-chord
   ;; https://github.com/abo-abo/hydra/issues/292#issuecomment-408130658
   (defun hydra-default-pre ()))
