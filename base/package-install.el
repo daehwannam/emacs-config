17b84f488380726c3e39fea136bb7efe8e8743ed
Daehwan Nam <dhnam@postech.ac.kr>
2023-02-17 00:00:43 +0900

    package install option update

--- base/package-install.el
+++ base/package-install.el
@@ -2,12 +2,23 @@
 (progn
   ;; package install
   ;; https://realpython.com/blog/python/emacs-the-best-python-editor/
-  (defvar dhnam/installable-domain-specific-packages) ; (defvar machine-domain-specific-packages some-value)
-  (setq dhnam/installable-domain-specific-packages (apply 'cl-concatenate (cons 'list (dhnam/machine-config-get-all 'installable-packages))))
+  (progn
+    (defvar dhnam/installable-domain-specific-packages nil)
+    (setq dhnam/installable-domain-specific-packages
+          (apply 'cl-concatenate (cons 'list (dhnam/machine-config-get-all 'installable-packages)))))
+
+  (progn
+    (defvar dhnam/installable-machine-specific-packages nil)
+    (let ((package-file-name "~/.emacs.d/config/packages.txt"))
+      (unless (file-exists-p package-file-name)
+        (write-region "(vlf magit)" nil package-file-name))
+      (setq dhnam/installable-machine-specific-packages
+            (car (read-from-string (dhnam/get-string-from-file package-file-name))))))
 
   (mapc (lambda (package)
           (if (consp package)
               (unless (package-installed-p (car package))
                 (quelpa package))
             (dhnam/install-package-unless-installed package)))
-	    dhnam/installable-domain-specific-packages))
+	    (append dhnam/installable-domain-specific-packages
+                dhnam/installable-machine-specific-packages)))
