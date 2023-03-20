0ad28cf6efc493cfd03a3e033ef5d947336c0186
Daehwan Nam <dhnam@postech.ac.kr>
2023-03-20 12:42:17 +0900

    vertico update

--- config/init/vertico.el
+++ config/init/vertico.el
@@ -142,7 +142,14 @@
       (defun consult-grep-on-default-directory (&optional dir initial)
         (interactive "P")
         (setq dir (or dir default-directory))
-        (consult--grep "Grep" #'consult--grep-builder dir initial))
+        (consult--grep
+         "Grep"
+         (cond
+          ((fboundp 'consult--grep-make-builder) #'consult--grep-make-builder)
+          ;; `consult--grep-builder' is used in old version of vertico
+          ((fboundp 'consult--grep-builder #'consult--grep-builder)))
+         dir
+         initial))
 
       (progn
         (comment (key-chord-define-global "js" 'consult-line))
