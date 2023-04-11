6f9a53d135c756d549b578bc8610ecdd5db068e8
Daehwan Nam <dhnam@postech.ac.kr>
2023-04-12 02:58:46 +0900

    minor update

--- config/machine-config.el
+++ config/machine-config.el
@@ -78,7 +78,7 @@
 
         (installable-packages
          ;; (base (vlf magit key-chord modalka evil avy hydra dash counsel multi-term))
-         (base (vlf magit dash multi-term counsel async wgrep which-key paredit poporg ctrlf expand-region company vterm clipetty el-patch avy))
+         (base (vlf magit dash multi-term counsel async wgrep which-key paredit poporg ctrlf expand-region company vterm clipetty el-patch avy puni))
          (documentation (org-contrib valign org-fragtog auctex latex-preview-pane pdf-tools define-word pdfgrep biblio))
          (vbox (vlf magit key-chord modalka evil dash  multi-term auctex latex-preview-pane material-theme elpy flycheck py-autopep8 blacken sphinx-doc workgroups2 perspective counsel iedit wgrep which-key counsel-projectile persp-projectile paredit markdown-mode tuareg ein hy-mode)) ; removed: highlight
          ;; (engels (vlf magit key-chord modalka evil dash elpy flycheck py-autopep8 blacken wgrep which-key projectile flx-ido))
