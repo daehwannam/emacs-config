60aa32952b3301454d7cac96d6422dece64a59c2
Daehwan Nam <dhnam@postech.ac.kr>
2023-06-12 07:18:00 +0900

    yasnippet is added

---
+++ config/init/yasnippet.el
@@ -0,0 +1,10 @@
+
+(require 'yasnippet)
+(require 'dhnam-yasnippet)
+
+(progn
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook #'yas-minor-mode)
+  (add-hook 'LaTeX-mode-hook #'yas-minor-mode))
+
+(provide 'init-yasnippet)
