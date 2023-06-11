b1186af048c9fe38880aa6d32ec19a1281eef8cc
Daehwan Nam <dhnam@postech.ac.kr>
2023-06-12 07:18:55 +0900

    minor update

--- config/init/yasnippet.el
+++ config/init/yasnippet.el
@@ -3,6 +3,7 @@
 (require 'dhnam-yasnippet)
 
 (progn
+  ;; https://github.com/joaotavora/yasnippet
   (yas-reload-all)
   (add-hook 'prog-mode-hook #'yas-minor-mode)
   (add-hook 'LaTeX-mode-hook #'yas-minor-mode))
