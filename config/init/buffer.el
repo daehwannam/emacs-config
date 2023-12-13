9161b57a2897ccc0189b0199f6f4713299ff7ec7
Daehwan Nam <dhnam@postech.ac.kr>
2023-12-13 11:06:48 +0900

    binding update

--- config/init/buffer.el
+++ config/init/buffer.el
@@ -3,9 +3,9 @@
 
 (progn
   (comment
-   ;; buffer-menu
-   ;; http://stackoverflow.com/questions/1231188/emacs-list-buffers-behavior
-   (global-set-key "\C-x\C-b" 'buffer-menu))
+    ;; buffer-menu
+    ;; http://stackoverflow.com/questions/1231188/emacs-list-buffers-behavior
+    (global-set-key "\C-x\C-b" 'buffer-menu))
   (progn
     ;; ibuffer
     ;; http://emacs.stackexchange.com/questions/202/close-all-dired-buffers
@@ -14,33 +14,33 @@
 (require 'dhnam-make-repeatable-command)
 
 (comment
- ;; other window with repeatition
- ;; http://stackoverflow.com/questions/91071/emacs-switch-to-previous-window
- ;;(load "make-repeatable-command") ;(load "~/.emacs.d/package/make-repeatable-command.el")
+  ;; other window with repeatition
+  ;; http://stackoverflow.com/questions/91071/emacs-switch-to-previous-window
+  ;;(load "make-repeatable-command") ;(load "~/.emacs.d/package/make-repeatable-command.el")
   
- ;; switch buffers
- ;; http://emacs.stackexchange.com/questions/728/how-do-i-switch-buffers-quickly
- ;;(global-set-key (kbd "C-c p") 'previous-buffer)
- ;;(global-set-key (kbd "C-c n") 'next-buffer)
+  ;; switch buffers
+  ;; http://emacs.stackexchange.com/questions/728/how-do-i-switch-buffers-quickly
+  ;;(global-set-key (kbd "C-c p") 'previous-buffer)
+  ;;(global-set-key (kbd "C-c n") 'next-buffer)
 
- ;;(global-set-key (kbd "C-c C-p") (make-repeatable-command 'previous-buffer))
- ;;(global-set-key (kbd "C-c C-n") (make-repeatable-command 'next-buffer))
+  ;;(global-set-key (kbd "C-c C-p") (make-repeatable-command 'previous-buffer))
+  ;;(global-set-key (kbd "C-c C-n") (make-repeatable-command 'next-buffer))
 
- ;;(global-set-key (kbd "C-M-p") (make-repeatable-command 'previous-buffer))
- ;;(global-set-key (kbd "C-M-n") (make-repeatable-command 'next-buffer))
+  ;;(global-set-key (kbd "C-M-p") (make-repeatable-command 'previous-buffer))
+  ;;(global-set-key (kbd "C-M-n") (make-repeatable-command 'next-buffer))
 
- ;;(global-set-key (kbd "C-c C-b") (make-repeatable-command 'previous-buffer))
- ;;(global-set-key (kbd "C-c C-f") (make-repeatable-command 'next-buffer))
+  ;;(global-set-key (kbd "C-c C-b") (make-repeatable-command 'previous-buffer))
+  ;;(global-set-key (kbd "C-c C-f") (make-repeatable-command 'next-buffer))
 
- ;; (global-set-key (kbd "ESC <f7>") (make-repeatable-command 'previous-buffer)) ; M-<f7>
- ;; (global-set-key (kbd "ESC <f8>") (make-repeatable-command 'next-buffer)) ; M-<f8>
+  ;; (global-set-key (kbd "ESC <f7>") (make-repeatable-command 'previous-buffer)) ; M-<f7>
+  ;; (global-set-key (kbd "ESC <f8>") (make-repeatable-command 'next-buffer)) ; M-<f8>
 
- ;; (global-set-key (kbd "<f7>") (make-repeatable-command 'previous-buffer)) ; M-<f7>
- ;; (global-set-key (kbd "<f8>") (make-repeatable-command 'next-buffer)) ; M-<f8>
+  ;; (global-set-key (kbd "<f7>") (make-repeatable-command 'previous-buffer)) ; M-<f7>
+  ;; (global-set-key (kbd "<f8>") (make-repeatable-command 'next-buffer)) ; M-<f8>
 
- ;; (global-set-key (kbd "C-x <left>") (make-repeatable-command 'previous-buffer)) ; M-<f7>
- ;; (global-set-key (kbd "C-x <right>") (make-repeatable-command 'next-buffer)) ; M-<f8>
- )
+  ;; (global-set-key (kbd "C-x <left>") (make-repeatable-command 'previous-buffer)) ; M-<f7>
+  ;; (global-set-key (kbd "C-x <right>") (make-repeatable-command 'next-buffer)) ; M-<f8>
+  )
 
 (global-set-key (kbd "C-x H") 'dhnam/copy-whole-buffer)
 
@@ -80,23 +80,32 @@
 (progn
   (require 'buffer-move nil t)
   (comment
-   (defhydra hydra-buffer-move ()
-     "buffer-move"
-     ("h" buf-move-left)
-     ("l" buf-move-right)
-     ("k" buf-move-up)
-     ("j" buf-move-down)
-     ("q" nil "quit"))
-   )
+    (defhydra hydra-buffer-move ()
+      "buffer-move"
+      ("h" buf-move-left)
+      ("l" buf-move-right)
+      ("k" buf-move-up)
+      ("j" buf-move-down)
+      ("q" nil "quit"))
+    )
 
   (progn
+    (comment
+      (defhydra hydra-buffer-move (global-map "C-c m")
+        "buffer-move"
+        ("q" nil "quit")
+        ("j" buf-move-left)
+        ("l" buf-move-right)
+        ("i" buf-move-up)
+        ("k" buf-move-down)))
+
     (defhydra hydra-buffer-move (global-map "C-c m")
       "buffer-move"
       ("q" nil "quit")
-      ("j" buf-move-left)
+      ("k" buf-move-left)
       ("l" buf-move-right)
       ("i" buf-move-up)
-      ("k" buf-move-down))
+      ("o" buf-move-down))
 
     (progn
       ;; Disable any hint message
