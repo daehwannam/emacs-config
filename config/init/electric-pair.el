bcd828c4e47f5ea6c8b6ffdd3dd5a39c76f8b472
Daehwan Nam <dhnam@postech.ac.kr>
2023-12-25 04:03:37 +0900

    electric-pair fix

--- config/init/electric-pair.el
+++ config/init/electric-pair.el
@@ -1,20 +1,32 @@
 
 (require 'dhnam-electric-pair)
 
-(with-eval-after-load 'elec-pair
+(progn
   ;; electric-pair-mode
-  (add-hook 'prog-mode-hook
-            (dhnam/hook-except-modes 'electric-pair-local-mode '(emacs-lisp-mode lisp-mode)))
 
-  (mapc (lambda (hook)
-          (add-hook hook 'electric-pair-local-mode))
-        '(inferior-python-mode-hook LaTeX-mode-hook org-mode-hook))
+  (progn
+    ;; Add electric-pair-local-mode to the hook for other modes
 
-  (mapc (lambda (hook)
-          (add-hook hook 'dhnam/set-python-electric-pairs))
-        '(python-mode-hook inferior-python-mode-hook))
+    ;; Modes that belongs to prog-mode
+    (add-hook 'prog-mode-hook
+              (dhnam/hook-except-modes 'electric-pair-local-mode '(emacs-lisp-mode lisp-mode)))
+
+    ;; Modes that don't belong to prog-mode
+    (mapc (lambda (hook)
+            (add-hook hook 'electric-pair-local-mode))
+          '(inferior-python-mode-hook LaTeX-mode-hook org-mode-hook)))
+
+  (progn
+    ;; Update pairs for Python
+    (mapc (lambda (hook)
+            (add-hook hook 'dhnam/set-python-electric-pairs))
+          '(python-mode-hook inferior-python-mode-hook)))
 
   (add-hook 'org-mode-hook 'dhnam/set-org-electric-pair-inhibit-predicate))
+
+(comment
+  (with-eval-after-load 'elec-pair
+   (progn)))
 
 
 (provide 'init-electric-pair)
