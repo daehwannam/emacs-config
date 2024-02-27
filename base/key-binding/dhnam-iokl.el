dd9ef87cdf02a2c113777feabf61fa3621e8c7e3
Daehwan Nam <dhnam@postech.ac.kr>
2024-02-27 09:59:01 +0900

    dhnam-iokl activation key update

--- base/key-binding/dhnam-iokl.el
+++ base/key-binding/dhnam-iokl.el
@@ -1,3 +1,7 @@
 
 (setq dhnam-iokl/activation-key dhnam/xcape-left-alt)
+
 (require 'dhnam-iokl)
+
+(dhnam-iokl/define-keys-for-bodies (kbd dhnam-iokl/activation-key))
+(dhnam-iokl/define-chords-for-bodies "wd")
