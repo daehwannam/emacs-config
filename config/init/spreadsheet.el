0614515ce7d59e95dc7b68ef942ed6d67c86016c
Daehwan Nam <dhnam@postech.ac.kr>
2024-04-19 19:49:09 +0900

    csv and tsv update

--- config/init/spreadsheet.el
+++ config/init/spreadsheet.el
@@ -1,3 +1,6 @@
+
+(require 'dhnam-spreadsheet)
+
 (progn
   ;; https://www.emacswiki.org/emacs/CsvMode
   ;; https://elpa.gnu.org/packages/csv-mode.html
@@ -5,6 +8,9 @@
     (add-hook 'csv-mode-hook #'csv-align-mode)
     (progn
       (require 'csv-mode)
-      (define-key csv-mode-map [(control ?c) (control ?l)] 'csv-align-mode))))
+      (comment (define-key csv-mode-map [(control ?c) (control ?l)] 'csv-align-mode))
+      (setq dhnam/csv-toggle-key (kbd "C-c C-l"))
+      (define-key csv-mode-map dhnam/csv-toggle-key 'dhnam/toggle-csv)
+      (define-key tsv-mode-map dhnam/csv-toggle-key 'dhnam/toggle-tsv))))
 
 (provide 'init-spreadsheet)
