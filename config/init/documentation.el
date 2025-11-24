c4df2e78152cea990e0cf5523f00982ae61c8353
Daehwan Nam <dhnam@postech.ac.kr>
2025-11-24 11:46:46 +0900

    binding update

--- config/init/documentation.el
+++ config/init/documentation.el
@@ -1,5 +1,14 @@
 
 (require 'dhnam-documentation)
+
+(global-set-key (kbd "C-c / /") 'poporg-dwim)
+(global-set-key (kbd "C-c / r") 'dhnam/pop-rst-dwim)
+
+(with-eval-after-load 'poporg
+  (define-key poporg-mode-map (kbd "<key-chord> qw k")  'dhnam/poporg-kill-buffer)
+  (define-key poporg-mode-map (kbd "<key-chord> wq k")  'dhnam/poporg-kill-buffer)
+  (when dhnam/exwm-cmd-line-arg-passed
+    (define-key poporg-mode-map (kbd "s-k") 'dhnam/poporg-kill-buffer-exit)))
   
 
 (provide 'init-documentation)
